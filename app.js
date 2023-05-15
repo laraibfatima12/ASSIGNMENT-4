ea78398ec8841c7d250509200b1d2a02176fabaa
laraibfatima12 <128951988+laraibfatima12@users.noreply.github.com>
2023-05-15 10:29:30 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+// ================= Js ASSIGNMENT 04 ===============//
+
+//==================== question 1 =================//
+
+// let name = "laraib fatima";  let age =18 ; let height = "5.6f";
+
+
+// =================  question  2 =============== //
+
+// legal variables
+
+//let my-name="laraib fatima"
+//let age -1= 19
+//let my-city="karachi"
+//let grade ="0ne"
+//let  phone number="03407841162"
+
+
+// =============== illegal variation =============== //
+
+//let mame="laraib fatima"; //number in start illegal
+//let my age=" 19 " // space in between illegal
+//let function=" Assalamualikum " //reserved keyword
+//let colony name="Majeed SRE" //use of # not allowed
+//let phone number="03407841162" //use of + , $ , _ are not allowed
+
+
+
+// ===================== question 3 ================= //
+
+
+//document.write (" <b> Rules for naming js variable </b> <br> <br>")
+
+
+//document.write ("variable names can only contain" + number + "," + $ + "and  + alphabet +", for example: $my-1stvariable"+"<br")
+
+
+//document.write ("variable must begin with a" + number + _ +"," + $ + "and  + alphabet +", for example: $name,_name or name "<br")
+
+
+//document.write ("variable name are case" +  sensitivity +"<br>"
+
+
+//document.write ("variable  names shoud not be JS "+ Keywords )
+
+
+
+
